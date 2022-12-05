96f91cd2d9669bdc02106afe5f0e13b4f2c93aba
markusrenoldner <m.renoldner@gmail.com>
2022-12-05 19:21:11 +0100

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,8 +2,19 @@
 #include <fstream>
 #include <iostream>
 
+void AddSubmatrix(mfem::SparseMatrix submatrix, mfem::SparseMatrix matrix, int rowoffset, int coloffset) {
+    for (int r = 0; r < submatrix.NumRows(); r++) {
+        mfem::Array<int> cols;
+        mfem::Vector srow;
+        submatrix.GetRow(r, cols, srow);
+        for (int c = 0; c < submatrix.NumCols(); c++) {
+            matrix.Add(rowoffset + r, coloffset + cols[c], srow[c]);
+        }
+        cols.DeleteAll();
+    }
+}
 
-void printvector(mfem::Vector vec, int stride=1) {
+void printvector2(mfem::Vector vec, int stride=1) {
     std::cout << std::endl<<"vec =\n";
     for (int j = 0; j<vec.Size(); j+=stride) {
         std::cout << std::setprecision(3) << std::fixed;
@@ -11,7 +22,28 @@ void printvector(mfem::Vector vec, int stride=1) {
         else {std::cout << vec[j];}
         std::cout << "\n";
     }
+    std::cout << "--------------\n";
 }
+
+void printvector(mfem::Vector vec, int stride=1) {
+    std::cout << std::endl<<"vec[::"<<stride<<"]=\n";
+    for (int j = 0; j<vec.Size(); j+=stride) {
+        std::cout << std::setprecision(3) << std::fixed;
+        std::cout << vec[j]<< "\n";
+    }
+    std::cout << "--------------\n";
+}
+
+void printvector3(mfem::Vector vec, int stride=1, int start=0, int stop=0, int prec=3) {
+    if (stop==0) { stop = vec.Size(); }
+    std::cout << std::endl<<"vec=\n";
+    for (int j = start; j<stop; j+=stride) {
+        std::cout << std::setprecision(prec) << std::fixed;
+        std::cout << vec[j]<< "\n";
+    }
+    std::cout << "\n";
+}
+
 
 void printmatrix(mfem::Matrix &mat) {
     for (int i = 0; i<mat.NumRows(); i++) {
