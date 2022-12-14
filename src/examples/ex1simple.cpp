00a56855e3c161d5a6523f609dbbf20ac5678a30
markusrenoldner <m.renoldner@gmail.com>
2022-12-14 16:35:46 +0100

    understand how true DOFs work

--- src/examples/ex1simple.cpp
+++ src/examples/ex1simple.cpp
@@ -7,14 +7,14 @@ using namespace mfem;
 
 
 
-
 int main(int argc, char *argv[]) {
 
     // mesh
-    const char *mesh_file = "extern/mfem-4.5/data/star.mesh";
+    // const char *mesh_file = "extern/mfem-4.5/data/star.mesh";
+    const char *mesh_file = "extern/mfem-4.5/data/ref-square.mesh";
     Mesh mesh(mesh_file, 1, 1);
     int dim = mesh.Dimension();
-    for (int l = 0; l < 2; l++) {mesh.UniformRefinement();}
+    for (int l = 0; l < 1; l++) {mesh.UniformRefinement();}
     
     // FE space
     int order = 1;
@@ -24,9 +24,13 @@ int main(int argc, char *argv[]) {
 
     // boundary conditions
     Array<int> ess_tdof_list;
-    Array<int> ess_bdr(mesh.bdr_attributes.Max());
-    ess_bdr = 0;
+    Array<int> ess_bdr(mesh.bdr_attributes.Max()); 
+    // mesh.bdr_attributes.Max() = 4
+    ess_bdr = 1;
+    // ess_bdr = {1,1,1,1}
     fespace.GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
+    // trueDOFs exclude e.g double-counted DOFs that are part of 2 elem
+    // ess_tdof_list = {0,1,2,3}
 
     // Linform
     LinearForm b(&fespace);
@@ -36,7 +40,7 @@ int main(int argc, char *argv[]) {
 
     // gridfunction
     GridFunction x(&fespace);
-    x = 0;
+    x = 0.;
 
     // blf
     BilinearForm a(&fespace);
@@ -48,7 +52,7 @@ int main(int argc, char *argv[]) {
 
     // solve
     GSSmoother M((SparseMatrix&)(*A));
-    PCG(*A, M, B, X, 1, 200, 1e-12, 0.0);
+    PCG(*A, M, B, X, 0, 200, 1e-12, 0.0);
 
     // visuals
     ofstream mesh_ofs("refined.mesh");
@@ -57,7 +61,6 @@ int main(int argc, char *argv[]) {
     ofstream sol_ofs("sol.gf");
     sol_ofs.precision(8);
     x.Save(sol_ofs);
-
     socketstream sol_sock("localhost", 19916);
     sol_sock.precision(8);
     sol_sock << "solution\n" << mesh << x << flush;
